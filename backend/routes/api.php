dac0035be71674cf4590480d3ca5cb2ef6121c38
Isabela Vitoria <isabelavitoriabarroso2004@gmail.com>
2025-04-01 23:03:35 -0300

    updates routes api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\ImageController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::put('/dataRelationship', [UpdateController::class, 'dataRelationship']);
-Route::post('/upload-image', [ImageController::class, 'store']);
-Route::get('/user-images/{userId}', [ImageController::class, 'getUserImages']);
+Route::put('/dataRelationship', [UpdateController::class, 'dataRelationship']); //salva informações sobre o relacionamento
+Route::post('/upload-image', [ImageController::class, 'store']); // carregaga o caminho das imagen spara o bd
+Route::get('/user-images/{userId}', [ImageController::class, 'getUserImages']); // recupera imagens
 
 
 //rota -> controller -> metodo no controller
