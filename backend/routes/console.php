811892debe85c25f1c461c0185c39ce92da8f2de
Isabela Vitoria <isabelavitoriabarroso2004@gmail.com>
2025-04-10 10:21:45 -0300

    Schedule a service at daily 8:00

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\SendBirthdayImage;
 
-Artisan::command('inspire', function () {
-    /** @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('send:birthday-images', function () {
+    \Log::info('[SERVICE] veio');
+    (new \App\Jobs\SendBirthdayImage())->handle(); 
+    $this->info('Job SendBirthdayImage despachado!');
+});
+
+Schedule::command('send:birthday-images')->dailyat('08:00')->timezone('America/Sao_Paulo');
