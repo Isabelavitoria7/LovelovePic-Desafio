c46f8f656d36f01dd0d5b5854a657df221f67591
Isabela Vitoria <isabelavitoriabarroso2004@gmail.com>
2025-04-10 10:20:18 -0300

    config class Kernel to process Schedule

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -7,27 +7,29 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Defina os comandos Artisan customizados, se houver.
-     */
-    protected $commands = [];
 
-    /**
-     * Agendamento de tarefas (jobs, comandos, etc).
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-        // Agendo o job SendBirthdayImage para rodar todo dia 
-        $schedule->job(new \App\Jobs\SendBirthdayImage)->dailyAt('08:00')->timezone('America/Sao_Paulo');
-    }
+    //eu preciso utilizar o metodo Schedule para que os comandos artisan personalizados passe por aqui para serem processados e guardados na lista de tarefas, mas lá em routes/console.php ja estou fazendo isso, e o Kernel já carrega
+    // protected function commands()
+    // {
+    //     require base_path('routes/console.php');
+    // }  automaticamente por baixo dos panos.
 
-    /**
-     * Registra os comandos Artisan para o app.
-     */
-    protected function commands(): void
-    {
-        $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
-    }
+    //como seria se eu fosse agendar por aqui
+    // protected $commands = [];
+
+    
+    // protected function schedule(Schedule $schedule): void
+    // {
+    //     // Agendo o job SendBirthdayImage para rodar todo dia 
+    //     $schedule->job(new \App\Jobs\SendBirthdayImage)->everyMinute()->timezone('America/Sao_Paulo');
+    // }
+
+    
+    // protected function commands(): void
+    // {
+    //     $this->load(__DIR__.'/Commands');
+
+    //     require base_path('routes/console.php');
+    // }
 }
