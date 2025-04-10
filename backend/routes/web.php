cb56e4b1fa06d46e19b4b5ffe013c9f44320b13d
Isabela Vitoria <isabelavitoriabarroso2004@gmail.com>
2025-04-10 10:23:08 -0300

    basic route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
+use App\Jobs\SendBirthdayImage;
 
-
+Route::get('/', function () {
+    return 'Running';
+});
