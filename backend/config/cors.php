ed8fb9f733eb50c068e72b89a6eb8da29e1e0306
isabela_vitoria7 <isabelavitoriabarroso2004@gmail.com>
2025-04-22 10:41:43 -0300

    update cors, config url front

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'https://lovelovepic-git-main-isabelavitoria7s-projects.vercel.app', 'https://lovelovepic-isabelavitoria7s-projects.vercel.app')],
 
     'allowed_origins_patterns' => [],
 
